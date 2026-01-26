e80b181e285fde2398d17604a823e7b91a3bb6c0
akshu-webdeveloper <akankshangupta0@gmail.com>
2026-01-26 17:33:13 +0000

    Conversion od data types

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -11,7 +11,7 @@ console.log(3
     let age = 19
     let isLoggedIn = false
 
-    // number =>2 to powe 53
+    // number =>2 to power 53
     // bigint
     //string =>""
     //boolean =>true/false
