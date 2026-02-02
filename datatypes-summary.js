505e401eae0527f0fc7bd7e703e014c202fc7dff
akshu-webdeveloper <akankshangupta0@gmail.com>
2026-02-02 17:49:24 +0000

    05_string

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -1,4 +1,5 @@
 //Primitive Data Types
+//
 //Call by value
 
 
@@ -7,7 +8,6 @@
 //java script cousin bhai "Typescript"
 const score = 100
 const scorVvalue = 100.3
-
 const isLoggedIn = false
 const outsideTem = null
 let userEmail;
@@ -15,7 +15,7 @@ let userEmail;
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 //const bigNumber = 345634667457765455787697n
 
 
@@ -33,7 +33,18 @@ const heros = ["shaktiman","naagraj","doge"]
     
 
 const myFunction = function(){
-    console.log("Hello World");
+    //console.log("Hello World");
 }
 
-console.log(typeof anotherId);
+//console.log(typeof anotherId);
+
+
+//+++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = "akshucreatesdotcom"
+let anothername = myYoutubename
+anothername = "thethingsof_18"
+console.log(myYoutubename);
+console.log(anothername);
